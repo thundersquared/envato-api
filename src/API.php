e55f88f40f421e41101dab6afd0979ac2925ab07
Anatoli Nicolae <nicolaeanatoli@gmail.com>
2018-02-02 09:29:57 +0100

    Missing args sorry 😬
    Signed-off-by: Anatoli Nicolae <nicolaeanatoli@gmail.com>

--- src/API.php
+++ src/API.php
@@ -31,7 +31,7 @@ class API {
       'headers' => [
         'Accept'        => 'application/json',
         'Authorization' => 'Bearer ' . $this->token,
-        'User-Agent'    => $this->userAgent || 'envato-api/0.0.1'
+        'User-Agent'    => $this->userAgent || 'envato-api/0.0.2'
       ]
     ];
 
@@ -48,150 +48,150 @@ class API {
    * Envato Market Catalog
    **/
 
-  public function getCollection($site, $args)
+  public function getCollection($site, $args = null)
   {
     return $this->makeRequest('/v3/market/catalog/collection', $args);
   }
 
-  public function getItem($site, $args)
+  public function getItem($site, $args = null)
   {
     return $this->makeRequest('/v3/market/catalog/item', $args);
   }
 
-  public function searchItems($site, $args)
+  public function searchItems($site, $args = null)
   {
     return $this->makeRequest('/v1/discovery/search/search/item', $args);
   }
 
-  public function searchComments($site, $args)
+  public function searchComments($site, $args = null)
   {
     return $this->makeRequest('/v1/discovery/search/search/comment', $args);
   }
 
-  public function getPopularItems($site, $args)
+  public function getPopularItems($site, $args = null)
   {
     return $this->makeRequest("/v1/market/popular:$site.json", $args);
   }
 
-  public function getCategories($site, $args)
+  public function getCategories($site, $args = null)
   {
-    return $this->makeRequest("/v1/market/categories:$site.json");
+    return $this->makeRequest("/v1/market/categories:$site.json", $args);
   }
 
-  public function getItemPrices($site, $args)
+  public function getItemPrices($site, $args = null)
   {
-    return $this->makeRequest("/v1/market/item-prices:$site.json");
+    return $this->makeRequest("/v1/market/item-prices:$site.json", $args);
   }
 
-  public function getNewItems($site, $category, $args)
+  public function getNewItems($site, $category, $args = null)
   {
-    return $this->makeRequest("/v1/market/new-files:$site,$category.json");
+    return $this->makeRequest("/v1/market/new-files:$site,$category.json", $args);
   }
 
-  public function getFeaturedItems($site, $args)
+  public function getFeaturedItems($site, $args = null)
   {
-    return $this->makeRequest("/v1/market/new-files:$site.json");
+    return $this->makeRequest("/v1/market/new-files:$site.json", $args);
   }
 
-  public function getRandomNewFiles($site, $args)
+  public function getRandomNewFiles($site, $args = null)
   {
-    return $this->makeRequest("/v1/market/random-new-files:$site.json");
+    return $this->makeRequest("/v1/market/random-new-files:$site.json", $args);
   }
 
   /**
    * User Details
    **/
 
-  public function getUserCollections($args)
+  public function getUserCollections($args = null)
   {
-    return $this->makeRequest('/v3/market/user/collections');
+    return $this->makeRequest('/v3/market/user/collections', $args);
   }
 
-  public function getPrivateCollection($args)
+  public function getPrivateCollection($args = null)
   {
-    return $this->makeRequest('/v3/market/user/collection');
+    return $this->makeRequest('/v3/market/user/collection', $args);
   }
 
-  public function getUsersDetails($username, $args)
+  public function getUsersDetails($username, $args = null)
   {
-    return $this->makeRequest("/v1/market/user:$username.json");
+    return $this->makeRequest("/v1/market/user:$username.json", $args);
   }
 
-  public function getUsersBadges($username, $args)
+  public function getUsersBadges($username, $args = null)
   {
     return $this->makeRequest("/v1/market/user-badges:$username.json");
   }
 
-  public function getUsersItems($username, $args)
+  public function getUsersItems($username, $args = null)
   {
-    return $this->makeRequest("/v1/market/user-items-by-site:$username.json");
+    return $this->makeRequest("/v1/market/user-items-by-site:$username.json", $args);
   }
 
-  public function getUsersNewItems($username, $site, $args)
+  public function getUsersNewItems($username, $site, $args = null)
   {
-    return $this->makeRequest("/v1/market/new-files-from-user:$username,$site.json");
+    return $this->makeRequest("/v1/market/new-files-from-user:$username,$site.json", $args);
   }
 
   /**
    * Private User Details
    **/
 
-  public function getSales($args)
+  public function getSales($args = null)
   {
-    return $this->makeRequest('/v3/market/author/sales');
+    return $this->makeRequest('/v3/market/author/sales', $args);
   }
 
-  public function getSaleByCode($args)
+  public function getSaleByCode($args = null)
   {
-    return $this->makeRequest('/v3/market/author/sale');
+    return $this->makeRequest('/v3/market/author/sale', $args);
   }
 
-  public function getPurchases($args)
+  public function getPurchases($args = null)
   {
-    return $this->makeRequest('/v3/market/buyer/list-purchases');
+    return $this->makeRequest('/v3/market/buyer/list-purchases', $args);
   }
 
-  public function getPurchaseByCode($args)
+  public function getPurchaseByCode($args = null)
   {
-    return $this->makeRequest('/v3/market/buyer/purchase');
+    return $this->makeRequest('/v3/market/buyer/purchase', $args);
   }
 
-  public function getPrivateUserDetails($args)
+  public function getPrivateUserDetails($args = null)
   {
-    return $this->makeRequest('/v1/market/private/user/account.json');
+    return $this->makeRequest('/v1/market/private/user/account.json', $args);
   }
 
-  public function getUsername($args)
+  public function getUsername($args = null)
   {
     return $this->makeRequest('/v1/market/private/user/username.json');
   }
 
-  public function getEmail($args)
+  public function getEmail($args = null)
   {
-    return $this->makeRequest('/v1/market/private/user/email.json');
+    return $this->makeRequest('/v1/market/private/user/email.json', $args);
   }
 
-  public function getSalesByMonth($args)
+  public function getSalesByMonth($args = null)
   {
-    return $this->makeRequest('/v1/market/private/user/earnings-and-sales-by-month.json');
+    return $this->makeRequest('/v1/market/private/user/earnings-and-sales-by-month.json', $args);
   }
 
   /**
    * Envato Market Stats
    **/
 
-  public function getTotalMarketUsers($args)
+  public function getTotalMarketUsers($args = null)
   {
-    return $this->makeRequest('/v1/market/total-users.json');
+    return $this->makeRequest('/v1/market/total-users.json', $args);
   }
 
-  public function getTotalMarketItems($args)
+  public function getTotalMarketItems($args = null)
   {
-    return $this->makeRequest('/v1/market/total-items.json');
+    return $this->makeRequest('/v1/market/total-items.json', $args);
   }
 
-  public function getTotalFilesBySite($site, $args)
+  public function getTotalFilesBySite($site, $args = null)
   {
-    return $this->makeRequest("/v1/market/number-of-files:$site.json");
+    return $this->makeRequest("/v1/market/number-of-files:$site.json", $args);
   }
 }
